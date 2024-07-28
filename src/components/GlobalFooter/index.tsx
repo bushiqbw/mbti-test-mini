f909cdfcb521232c9dbcfec27184459da51d3682
bushiqbw <1036279449@qq.com>
2024-07-28 14:44:50 +0800

    mbit-test-mini

---
+++ src/components/GlobalFooter/index.tsx
@@ -0,0 +1,15 @@
+import { View } from "@tarojs/components";
+import "./index.scss";
+
+/**
+ * 全局底部栏组件
+ * @author <a href="https://github.com/bushiqbw">qbw</a>
+ *
+ */
+export default () => {
+  return (
+    <View className="globalFooter">
+      作者：程序员鱼皮
+    </View>
+  );
+};
